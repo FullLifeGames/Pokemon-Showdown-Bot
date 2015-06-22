e22443819bc7d7ed0170ab648b01e722fc553cce
FullLifeGames <fulllifegames@gmail.com>
2015-06-22 10:08:09 +0200

    Update Fighter.cs

--- Pokemon Showdown Bot/Fighter.cs
+++ Pokemon Showdown Bot/Fighter.cs
@@ -163,7 +163,6 @@ namespace Pokemon_Showdown_Bot
             Debug.WriteLine("Picking Start Pokemon");
             pickStartPokemon();
 
-            Debug.WriteLine("Wait For Opponent");
             while (waitingForOpponent() || skippingTurnWaiting())
             {
                 Thread.Sleep(50);
@@ -177,20 +176,17 @@ namespace Pokemon_Showdown_Bot
                 megaEvolveIfPossible();
                 Debug.WriteLine("Make A Move");
                 makeMove();
-                Debug.WriteLine("Wait For Opponent");
                 while (waitingForOpponent() || skippingTurnWaiting())
                 {
                     Thread.Sleep(50);
                 }
                 meltPokemonWithNumbers();
                 pickPokemonifDefeated();
-                Debug.WriteLine("Wait For Opponent");
                 while (waitingForOpponent() || skippingTurnWaiting())
                 {
                     Thread.Sleep(50);
                 }
                 pickPokemonifDefeated();
-                Debug.WriteLine("Wait For Opponent");
                 while (waitingForOpponent() || skippingTurnWaiting())
                 {
                     Thread.Sleep(50);
